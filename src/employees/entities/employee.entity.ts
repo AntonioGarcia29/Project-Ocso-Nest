96ffc3fab61fe90db22e2c50e75b5125ac05876a
Antonio Iván García Galván <agarcia261@alumnos.uaq.mx>
2024-11-10 17:56:42 -0600

    Entity Fk

--- src/employees/entities/employee.entity.ts
+++ src/employees/entities/employee.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Location } from "src/locations/entities/location.entity";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 @Entity()
 export class Employee {
     @PrimaryGeneratedColumn('uuid')
@@ -17,4 +18,9 @@ export class Employee {
     })
     photoURL: string
     
+    @ManyToOne(()=> Location, (location) => location.employees)
+    @JoinColumn({
+        name: "locationId"
+    })
+    location: Location;
 }
