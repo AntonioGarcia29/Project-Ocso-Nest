94f68395b02d06d9042051cb567dea8d1607cdd3
Antonio Iván García Galván <agarcia261@alumnos.uaq.mx>
2024-12-08 17:50:01 -0600

    Auth in all routes

--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -8,16 +8,18 @@ import { User } from 'src/auth/entities/user.entity';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ROLES } from 'src/auth/constants/roles.constants';
 
 @Controller('providers')
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
+  @Auth(ROLES.MANAGER)
 
   @Post()
   create(@Body() createProviderDto: CreateProviderDto) {
     return this.providersService.create(createProviderDto);
   }
-
+  @Auth(ROLES.MANAGER, ROLES.EMPLOYEE)
   @Get(':name')
   async findByName(@Param('name') name: string) {
     const providerName = await this.providersService.findOneByName(name);
@@ -25,8 +27,7 @@ export class ProvidersController {
       return providerName
   }
 
-  @Auth("Employee")
-
+  @Auth(ROLES.MANAGER, ROLES.EMPLOYEE)
   @Get()
   findAll(@UserData() user: User) {
     console.log(user);
@@ -36,19 +37,20 @@ export class ProvidersController {
     return this.providersService.findAll();
   }
   
-   
- 
+  @Auth(ROLES.MANAGER, ROLES.EMPLOYEE)
   @Get(':id')
   async findOne(@Param('id') id: string) {
     const provider = await this.providersService.findOne(id);
     if (!provider) throw new NotFoundException()
       return provider
   }
+  @Auth(ROLES.MANAGER)
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProviderDto: UpdateProviderDto) {
     return this.providersService.update(id, updateProviderDto);
   }
+  @Auth(ROLES.MANAGER)
 
   @Delete(':id')
   remove(@Param('id') id: string) {
